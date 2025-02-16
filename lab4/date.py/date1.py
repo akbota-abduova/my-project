c10c91e184dfaa44a1999429a3506de79103dee5
akbota-abduova <a_abduova@kbtu.kz>
2025-02-16 16:51:07 +0600

    added some chages

--- lab4/date.py/date1.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta
-
-now = datetime.now()
-five_days_ago = now - timedelta(days=5)
-
-print(f"today: {now.strftime('%Y-%m-%d')}")
-print(f"5 days ago: {five_days_ago.strftime('%Y-%m-%d')}")
--- lab4/date.py/date3.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-time = datetime.datetime.now()
-print("Current time with microseconds: ", time.time())
-print("Current time without microsecond: ", time.strftime("%X"))
