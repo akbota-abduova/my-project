321ec72953764794540053a7da6ce28ee919aa74
akbota-abduova <a_abduova@kbtu.kz>
2025-03-16 06:54:09 +0600

    changes

--- lab7/task1/clock.py
+++ lab7/task1/clock.py
@@ -1,37 +1,50 @@
 import pygame
+import time
 
-# Инициализация Pygame
 pygame.init()
 
-# Загрузка изображений
-mickey_image = pygame.image.load('mickey_hands.png')
-right_hand = pygame.image.load('right_hand.png')
-left_hand = pygame.image.load('left_hand.png')
+mickey_image = pygame.image.load('C:/Users/user/Downloads/mickey_hands.png')
+right_hand = pygame.image.load('C:/Users/user/Downloads/right_hand.png')
+left_hand = pygame.image.load('C:/Users/user/Downloads/left_hand.png')
 
-# Уменьшаем размер изображений для лучшего отображения
-mickey_image = pygame.transform.scale(mickey_image, (200, 200))  # Изменение размера изображения Микки
-right_hand = pygame.transform.scale(right_hand, (50, 50))  # Изменение размера правой руки
-left_hand = pygame.transform.scale(left_hand, (50, 50))  # Изменение размера левой руки
+mickey_image = pygame.transform.scale(mickey_image, (1000, 900))  
+right_hand = pygame.transform.scale(right_hand, (1800, 300))  
+left_hand = pygame.transform.scale(left_hand, (500, 100)) 
 
 # Создание окна
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((1000, 600))
 
 # Настройка таймера
 clock = pygame.time.Clock()
 
-# Главный цикл
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
 
-    # Отображаем фон и изображения
-    screen.fill((255, 255, 255))  # Белый фон
-    screen.blit(mickey_image, (100, 100))  # Рисуем Микки
-    screen.blit(right_hand, (300, 300))  # Минутная стрелка
-    screen.blit(left_hand, (400, 400))  # Секундная стрелка
+    # Получаем текущее время
+    seconds = int(time.time()) % 60  
+    minutes = (int(time.time()) // 60) % 60  
 
-    pygame.display.flip()  # Обновление экрана
+    second_angle = 6 * seconds  # 360 градусов за 60 секунд
+    minute_angle = 6 * minutes  # 360 градусов за 60 минут
 
-    clock.tick(60)  # Ограничиваем FPS до 60
+    screen.fill((255, 255, 255))
+
+    screen.blit(mickey_image, (400 - mickey_image.get_width() // 2, 300 - mickey_image.get_height() // 2))  # Центрируем Микки
+
+    # Вращаем стрелки
+    rotated_right_hand = pygame.transform.rotate(right_hand, -minute_angle)
+    rotated_left_hand = pygame.transform.rotate(left_hand, -second_angle)
+
+    right_hand_rect = rotated_right_hand.get_rect(center=(400, 300))  # Центр для правой руки
+    left_hand_rect = rotated_left_hand.get_rect(center=(400, 300))  # Центр для левой руки
+
+    # Отображаем стрелки
+    screen.blit(rotated_right_hand, right_hand_rect)  # Минутная стрелка
+    screen.blit(rotated_left_hand, left_hand_rect)  # Секундная стрелка
+
+    pygame.display.flip()  # Обнова экрана
+
+    clock.tick(60)  # FPS 60
